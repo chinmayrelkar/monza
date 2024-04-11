94455ae06650b5ee7db50aceaf6c3d1f7b91a7ad
Chinmay Relkar <78363244+chinmayrelkar@users.noreply.github.com>
2024-04-12 02:24:01 +0530

    Adds monza lib

---
+++ monza.go
@@ -0,0 +1,115 @@
+
+package monza
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+)
+
+
+type Destination interface{
+	Setup(ctx context.Context) error
+	Record(ctx context.Context, event Event)
+	Teardown(ctx context.Context)
+}
+
+
+type ServiceID string
+
+type Event struct {
+	Event      string      `json:"event,omitempty"`
+	UserID     string      `json:"user_id,omitempty"`
+	UserEmail  string      `json:"user_email,omitempty"`
+	DocumentID string      `json:"document_id,omitempty"`
+	RequestID  string      `json:"request_id,omitempty"`
+	SessionID  string      `json:"session_id,omitempty"`
+	Measure1   int64       `json:"measure_1,omitempty"`
+	Measure2   int64       `json:"measure_2,omitempty"`
+	Data       interface{} `json:"data,omitempty"`
+	ID         int64       `json:"id,omitempty"`
+	ClientTime time.Time   `json:"client_time,omitempty"`
+	IPAddr     string      `json:"ip_addr"`
+	ServiceID ServiceID 		`json:"service_id"`
+}
+
+func (e Event) JSON() []byte {
+	eventAsJSONStringBytes, err := json.Marshal(e)
+	if err != nil {
+		return nil
+	}
+	return eventAsJSONStringBytes
+}
+
+
+
+type Client interface {
+	RegisterDestination(ctx context.Context, destination Destination) error
+	Record(events Event)
+	Teardown(ctx context.Context)
+}
+
+var instance Client
+
+func Get(ctx context.Context, config Config) Client {
+	if instance != nil {
+		return instance
+	}
+	c := &client{
+		Config: config,
+		listener: make(chan Event),
+		quit: make(chan interface{}),
+	}
+	c.startListening(ctx)
+	instance = c
+	return instance
+}
+
+
+type Config struct {
+	IpAddress    string
+	Destinations []Destination
+}
+
+type client struct {
+	listener chan Event
+	Config
+	quit chan interface{}
+}
+
+
+
+func (c *client) RegisterDestination(ctx context.Context, destination Destination) error {
+	err := destination.Setup(ctx)
+	if err != nil {
+		return err
+	}
+	c.Destinations = append(c.Destinations, destination)
+	return nil
+}
+
+func (c *client) Record(event Event) {
+	c.listener <- event
+}
+
+// Teardown implements Client.
+func (c *client) Teardown(ctx context.Context) {
+	c.quit <- nil
+	for _, destination := range c.Destinations{
+		destination.Teardown(ctx)
+	}
+}
+
+func (c *client) startListening(ctx context.Context) {
+	for {
+		select {
+		case <- c.quit:
+			return
+		default:
+			events := <-c.listener
+			for _, destination := range c.Destinations {
+				destination.Record(ctx, events)
+			}
+		}
+	}
+}
